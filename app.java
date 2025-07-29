e95010d7c03eeee7c4dac81c660de374ded5d79c
siva <siva@gmail.com>
2025-07-29 06:22:01 +0000

    code added by siva

---
+++ app.java
@@ -0,0 +1,14 @@
+// Java Program to Print
+// Hello World
+import java.io.*;
+
+// Driver Class
+class GFG {
+    // main function
+    public static void main(String[] args)
+    {
+
+        // Printing Hello World
+        System.out.println("Hello World!");
+    }
+}
